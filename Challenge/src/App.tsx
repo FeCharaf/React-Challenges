592da3b5a864b87ea4bbc1bedc1e07dfa6a70fc6
FeCharaf <felipecharaf@gmail.com>
2025-01-24 13:09:31 -0300

    Undo button

--- Challenge/src/App.tsx
+++ Challenge/src/App.tsx
@@ -8,28 +8,54 @@ type Circle = {
 
 function App() {
   const [list, setList] = useState<Circle[]>([]);
+  const [removed, setRemoved] = useState<Circle[]>([]);
 
   function handleOnClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
     let x = event.pageX;
     let y = event.pageY;
 
-    var newCircle : Circle = {
-      pageX : x - 25,
-      pageY : y - 25,
+    var newCircle: Circle = {
+      pageX: x - 25,
+      pageY: y - 25,
     };
 
     setList((prev) => [...prev, newCircle]);
   }
 
+  function handleUndo(event) {
+    event.stopPropagation();
+
+    var removedItem:Circle = list.slice((-1))
+    
+    setRemoved((prev) => [...prev, removedItem])
+    
+    var newList = list.slice(0, -1)
+    setList(newList)
+  }
+
+  function handleRedo(event) {
+    event.stopPropagation();
+  }
+
   return (
     <div onClick={handleOnClick} id="Home">
-      {list.map((item:Circle, index) => (
-        <span key={index} className="Circle" style={{top: `${item.pageY}px`, left: `${item.pageX}px`}}></span>
+      {list.map((item: Circle, index) => (
+        <span
+          key={index}
+          className="Circle"
+          style={{ top: `${item.pageY}px`, left: `${item.pageX}px` }}
+        ></span>
       ))}
-      <h1>Challenge</h1>
+      <div className="btn_div">
+        <button className="btn" onClick={handleUndo}>
+          ↩ UNDO
+        </button>
+        <button className="btn" onClick={handleRedo}>
+          ↪ REDO
+        </button>
+      </div>
     </div>
   );
 }
 
 export default App;
-
